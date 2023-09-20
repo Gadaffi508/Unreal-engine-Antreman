ebd06611b178394909d4610eee2321d8fbc906ea
Yusuf <yusuf_zeki1234@hotmail.com>
2023-09-20 19:57:17 +0300

    first video try added

---
+++ MyProject/Intermediate/Build/Win64/UE4Editor/Inc/MyProject/NewActorComponent.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYPROJECT_NewActorComponent_generated_h
+#error "NewActorComponent.generated.h already included, missing '#pragma once' in NewActorComponent.h"
+#endif
+#define MYPROJECT_NewActorComponent_generated_h
+
+#define MyProject_Source_MyProject_NewActorComponent_h_13_SPARSE_DATA
+#define MyProject_Source_MyProject_NewActorComponent_h_13_RPC_WRAPPERS
+#define MyProject_Source_MyProject_NewActorComponent_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define MyProject_Source_MyProject_NewActorComponent_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUNewActorComponent(); \
+	friend struct Z_Construct_UClass_UNewActorComponent_Statics; \
+public: \
+	DECLARE_CLASS(UNewActorComponent, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(UNewActorComponent)
+
+
+#define MyProject_Source_MyProject_NewActorComponent_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUNewActorComponent(); \
+	friend struct Z_Construct_UClass_UNewActorComponent_Statics; \
+public: \
+	DECLARE_CLASS(UNewActorComponent, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(UNewActorComponent)
+
+
+#define MyProject_Source_MyProject_NewActorComponent_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UNewActorComponent(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UNewActorComponent) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UNewActorComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UNewActorComponent); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UNewActorComponent(UNewActorComponent&&); \
+	NO_API UNewActorComponent(const UNewActorComponent&); \
+public:
+
+
+#define MyProject_Source_MyProject_NewActorComponent_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UNewActorComponent(UNewActorComponent&&); \
+	NO_API UNewActorComponent(const UNewActorComponent&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UNewActorComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UNewActorComponent); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UNewActorComponent)
+
+
+#define MyProject_Source_MyProject_NewActorComponent_h_13_PRIVATE_PROPERTY_OFFSET
+#define MyProject_Source_MyProject_NewActorComponent_h_10_PROLOG
+#define MyProject_Source_MyProject_NewActorComponent_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject_Source_MyProject_NewActorComponent_h_13_PRIVATE_PROPERTY_OFFSET \
+	MyProject_Source_MyProject_NewActorComponent_h_13_SPARSE_DATA \
+	MyProject_Source_MyProject_NewActorComponent_h_13_RPC_WRAPPERS \
+	MyProject_Source_MyProject_NewActorComponent_h_13_INCLASS \
+	MyProject_Source_MyProject_NewActorComponent_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MyProject_Source_MyProject_NewActorComponent_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject_Source_MyProject_NewActorComponent_h_13_PRIVATE_PROPERTY_OFFSET \
+	MyProject_Source_MyProject_NewActorComponent_h_13_SPARSE_DATA \
+	MyProject_Source_MyProject_NewActorComponent_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	MyProject_Source_MyProject_NewActorComponent_h_13_INCLASS_NO_PURE_DECLS \
+	MyProject_Source_MyProject_NewActorComponent_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MYPROJECT_API UClass* StaticClass<class UNewActorComponent>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MyProject_Source_MyProject_NewActorComponent_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
